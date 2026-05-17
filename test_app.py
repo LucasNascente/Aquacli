5495f1342a18a8710640e4e8e17d11ed88c3cae3
Lucas Ribeiro Nascente <147668620+LucasNascente@users.noreply.github.com>
2026-05-17 19:34:43 -0300

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,6 +1,7 @@
 import pytest
 from app import AquaCli
-
+from unittest.mock import patch
+from app import buscar_temperatura_local, ajustar_meta_agua
 
 def test_add_water_success():
     tracker = AquaCli()
@@ -19,9 +20,6 @@ def test_check_status_goal_reached():
     tracker.add_water(2000)
     msg = "Parabéns! Você atingiu sua meta diária!"
     assert tracker.check_status() == msg
-from unittest.mock import patch
-from app import buscar_temperatura_local, ajustar_meta_agua
-
 @patch('app.requests.get')
 def test_integracao_openweather_api(mock_get):
     # Simulamos a resposta da API dizendo que faz 32.5 graus
